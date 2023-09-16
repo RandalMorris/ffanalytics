ec81d261ca8c2de036d52e465685a54622f571d4
Randal Morris <morrrand@gmail.com>
2023-09-15 22:43:28 -0500

    Add Weekly graphs

--- R/helper_funcs.R
+++ R/helper_funcs.R
@@ -207,6 +207,190 @@ extract_src_scrapes_from_scrape = function(data_result) {
 
 
 
+#Functions for weekly charts
+
+.scrape_fantasyfootballnerd = function(week = ffanalytics:::get_scrape_week(), data = my_scrape) {
+
+  cat("",sep = "\n")
+  cat("Scraping Fantasy Nerd Weekly")
+  FantasyNerds_QB = read.csv(paste0("C:/Users/morrr/Downloads/FantasyNerds_Week",week,"_QB_Projections.csv"),na = "0")
+  FantasyNerds_RB = read.csv(paste0("C:/Users/morrr/Downloads/FantasyNerds_Week",week,"_RB_Projections.csv"),na = "0")
+  FantasyNerds_WR = read.csv(paste0("C:/Users/morrr/Downloads/FantasyNerds_Week",week,"_WR_Projections.csv"),na = "0")
+  FantasyNerds_TE = read.csv(paste0("C:/Users/morrr/Downloads/FantasyNerds_Week",week,"_TE_Projections.csv"),na = "0")
+
+  FantasyNerds_QB$position = "QB"
+  FantasyNerds_RB$position = "RB"
+  FantasyNerds_WR$position = "WR"
+  FantasyNerds_TE$position = "TE"
+
+  FantasyNerds_Final = plyr::rbind.fill(FantasyNerds_QB, FantasyNerds_RB, FantasyNerds_WR,
+                                        FantasyNerds_TE)
+  names(FantasyNerds_Final) = tolower(names(FantasyNerds_Final))
+
+  rm(FantasyNerds_QB, FantasyNerds_RB, FantasyNerds_WR, FantasyNerds_TE)
+
+  FantasyNerds_Final = FantasyNerds_Final %>%
+    mutate(src_id = gsub("[[:punct:]]|\\s+", "-", tolower(player)),
+           data_src = "FantasyNerds") %>%
+    mutate(src_id = gsub("--", "-", tolower(src_id))) %>%
+    mutate(id = ffanalytics:::player_ids$id[match(.$src_id,ffanalytics:::player_ids$numfire_id)]) %>%
+    mutate(id2 = ffanalytics:::player_ids$id[match(.$src_id,ffanalytics:::player_ids$fantasypro_id)]) %>%
+    mutate(id = coalesce(id, id2)) %>%
+    select(-c(id2))
+
+  stat_cols = c(id = "id", src_id = "src_id", data_src = "data_src",
+                player = "player", team = "team", pos = "position",
+                pass_att = "pass.att", pass_comp = "pass.cmp",
+                pass_yds = "pass.yds", pass_tds = "pass.td", pass_int = "int",
+                rush_att = "rush.att", fumbles_lost = "fmbl",
+                rush_yds = "rush.yds", rush_tds = "rush.td",
+                rec = "rec", rec_yds ="rec.yds",rec_tds = "rec.td")  %>%
+    plyr::ldply(., data.frame) %>%
+    dplyr::rename(., match = .id, raw = X..i..)
+  typ_cols = cols(id = col_character(), player = col_character(), team = col_character(),
+                  pos = col_character(), pass_att = col_double(), pass_comp = col_double(),
+                  pass_yd = col_double(), pass_td = col_double(),
+                  rush_yd = col_double(), rush_td = col_double(),
+                  rec = col_double(), rec_yd = col_double(), rec_td = col_double(),  fumbles_lost = col_double(),
+                  data_src = col_character(), src_id = col_character())
+
+  FantasyNerds_Final = FantasyNerds_Final %>% select(stat_cols$raw) %>%
+    type_convert(., col_types = typ_cols)
+
+  names(FantasyNerds_Final) = stat_cols$match[match(names(FantasyNerds_Final), stat_cols$raw)]
+
+  FantasyNerds_Final = split(FantasyNerds_Final,FantasyNerds_Final$pos)
+
+
+  for(scr_pos in names(data)) {
+    data[[scr_pos]] = bind_rows(data[[scr_pos]], FantasyNerds_Final[[scr_pos]])
+  }
+  return(data)
+}
+
+.scrape_sleeper = function(data = my_scrape) {
+  ### Get Sleeper Data
+  cat("",sep = "\n")
+  cat("Scraping Sleeper Weekly")
+  baseurl="https://api.sleeper.com/projections/nfl/2023/"
+  positions= "&position[]=QB&position[]=RB&position[]=WR&position[]=TE,&position[]=K,&position[]=DEF"
+  url = paste0(baseurl,ffanalytics:::get_scrape_week(),"?season_type=regular",positions)
+
+  sleeper_ids <- ffscrapr::sleeper_players() %>%
+    filter(status == "Active", pos %in% c("QB", "RB", "WR", "TE"), team != "FA") %>%
+    dplyr::rename(src_id = player_id, name = player_name) %>%
+    mutate(data_src = "Sleeper",id = ffanalytics:::player_ids$id[match(.$src_id,ffanalytics:::player_ids$sleeper_id)]) %>%
+    select(id, data_src, src_id, name, pos, team)
+
+  Sleeper = httr::VERB(verb = "GET",
+                       url = url) %>%
+    httr::content("parsed", "application/json") %>%
+    lapply(., unlist) %>% dplyr::bind_rows(.) %>% select(-c(player.team, team))
+  names(Sleeper) <- gsub(x = names(Sleeper), pattern = "player.|stats.|metadata.", replacement = "")
+
+  Sleeper = Sleeper %>% dplyr::rename(src_id = id) %>%
+    inner_join(sleeper_ids, by = c("src_id" = "src_id")) %>% relocate(c(id,data_src, name, pos, team), .before = gp) %>%
+
+    select(-c(rec_0_4,rec_5_9,rec_10_19,rec_20_29, rec_30_39, rec_40p),
+           -starts_with("adp_"),-starts_with("pts_"), -starts_with("xp"), -starts_with("fg"),
+           -starts_with("idp_"), -ends_with("_fd"), -starts_with("bonus_"), -starts_with("injury_"))
+
+
+  stat_cols = c(id = "id", src_id = "src_id", data_src = "data_src", player = "name", team = "team",
+                pos = "pos", pass_comp = "pass_cmp", pass_att = "pass_att", pass_yds = "pass_yd",
+                pass_comp_pct = "cmp_pct",
+                pass_tds = "pass_td", pass_int = "pass_int",rush_att = "rush_att",
+                rush_yds = "rush_yd", rush_tds = "rush_td", fumbles_lost =  "fum_lost",
+                two_pts = "two_pts", rec = "rec",
+                rec_yds ="rec_yd",rec_tds = "rec_td") %>%
+    plyr::ldply(., data.frame) %>%
+    dplyr::rename(., match = .id, raw = X..i..)
+
+  typ_cols = cols(id = col_character(), player = col_character(), team = col_character(),
+                  pos = col_character(), cmp_pct = col_double(), pass_cmp = col_double(),
+                  pass_att = col_double(), pass_yd = col_double(), pass_td = col_double(),
+                  rush_att = col_double(), rush_yd = col_double(), rush_td = col_double(),
+                  rec = col_double(), rec_att = col_double(), rec_yd = col_double(), rec_td = col_double(),
+                  data_src = col_character(), src_id = col_character())
+
+  Sleeper = Sleeper %>% dplyr::mutate(two_pts = sum(as.integer(c_across(ends_with("_2pt"))))) %>%
+    select(stat_cols$raw) %>% type_convert(., col_types = typ_cols)
+
+  names(Sleeper) <- stat_cols$match[match(names(Sleeper), stat_cols$raw)]
+  Sleeper = Sleeper[!is.na(names(Sleeper))]
+  Sleeper = split(Sleeper, Sleeper$pos)
+
+  for(scr_pos in names(data)) {
+    data[[scr_pos]] = bind_rows(data[[scr_pos]], Sleeper[[scr_pos]])
+  }
+
+  return(data)
+}
+
+
+
+#'Create Graphs for user
+.Graphs <- function(){
+
+
+  .graph_func(pos="QB", pos_txt="Quarter Back", rlim = 32, xlim=25)
+  .graph_func(pos="RB", pos_txt="Running Back", rlim = 75, xlim=25)
+  .graph_func(pos="WR", pos_txt="Wide Receiver", rlim = 75, xlim=25)
+  .graph_func(pos="TE", pos_txt="Tight End", rlim = 40, xlim=25)
+  .graph_func(pos="K", pos_txt="Kicker", rlim = 32, xlim=25)
+  .graph_func(pos="DST", pos_txt="Defense", rlim = 32, xlim=25)
+
+}
+
+#'Base Graph Set
+
+.graph_func <- function(pos, pos_txt, rlim, xlim){
+
+  season = ffanalytics:::get_scrape_year()
+  varweek = ffanalytics:::get_scrape_week()
+  usecase = "NarFFL"
+
+  ffproj <<- my_projections %>%
+    dplyr::filter(position == pos & !is.na(floor)) %>%
+    dplyr::slice_head(n = rlim)
+
+  #geom_point(aes(x=Total_Points), size =2, shape=1, color = "black") +
+  fp_plot <<- ggplot(ffproj, aes(x=points, y=pos_rank, color=factor(tier))) +
+    geom_errorbarh(aes(xmin=floor,xmax=ceiling), height=.3) +
+    geom_point(size=5,color="white")+
+    geom_text(aes(x=points,label=round(points,0)),check_overlap = T,size=4) +
+    geom_text(aes(x=ceiling, label=paste0(pos_rank, " | ",name," | ",uncertainty),
+                  hjust=-.25, vjust=(.5), angle=(0), size=1),check_overlap = T, size=4) +
+    geom_text(aes(x=mean(points), y= 0, label=paste("Average Uncertainty = ",
+                                                    round(mean(uncertainty,na.rm = TRUE),2),sep = "")),color="black") +
+    geom_text(aes(x=round(max(points),0), y= -2,
+                  label=paste("Mean Projection | Rank | Player | Uncertainty",sep = ""),
+                  size=1),check_overlap = T,color="black") +
+    theme(
+      panel.background = element_rect(fill = "white",
+                                      colour = "lightblue",
+                                      size = 0.5, linetype = "solid"),
+      panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                      colour = "white"),
+      panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                      colour = "white")
+      ,plot.title = element_text(hjust = 0.5)
+      #,panel.grid.major.x = element_blank()
+      #,panel.grid.minor.y = element_blank()
+      ,panel.border=element_rect(color="grey",fill=NA)
+      ,legend.position = "none") +
+    scale_y_reverse() +
+    ylab("Average Rank") + xlab("Median FPTS Projection") +
+    coord_cartesian(xlim=c(0,(max(ffproj$ceiling)+xlim)))
+
+
+  fp_plot + labs(title = paste(season, "Season Week", varweek, pos_txt, "Projections", usecase, date()))
+
+  ggsave(paste0(getwd(),"/Visualizations/",season,
+                "/Week ",varweek,"/",pos_txt,".png"),
+         device = "png",width = 14, height = 11,dpi = 320, scale = 1, units = "in")
+
+}
 
 
 
---
+++ data-raw/weekly_charts.R
@@ -0,0 +1,38 @@
+
+suppressPackageStartupMessages(
+  {
+  library(ffanalytics)
+  library(dplyr)
+  library(ggplot2)
+  library(readr)
+    }
+  )
+
+NarFFL_Scoring <- AnalyticsFootball::scoring_rules[[paste0("NarFFL")]]
+
+my_scrape <- ffanalytics::scrape_data(src = c("CBS", "ESPN", "FantasyPros", "FantasySharks", "FFToday",
+                                              "NumberFire", "NFL"),
+                         pos = c("QB", "RB", "WR", "TE", "K", "DST"),
+                         season = NULL, # NULL grabs the current season
+                         week = NULL) # NULL grabs the current week
+
+
+my_scrape1 = my_scrape %>%
+  ffanalytics::.scrape_sleeper(data = .) %>%
+  ffanalytics::.scrape_fantasyfootballnerd(data = .)
+
+my_projections <<- ffanalytics::projections_table(my_scrape, avg_type = "average",
+                                                  scoring_rules = NarFFL_Scoring) %>%
+  ffanalytics::add_player_info() %>%
+  ffanalytics::add_ecr() %>%
+  ffanalytics::add_uncertainty() %>%
+  tidyr::unite("first_name", first_name:last_name,sep = " ") %>% rename(name = first_name) %>%
+  dplyr::select(-c(exp, pos,avg_type)) %>%
+  dplyr::mutate_if(is.numeric, ~replace(., is.na(.), 0)) %>%
+  dplyr::mutate_if(is.numeric, round, digits=2) %>%
+  dplyr::arrange(position,pos_rank) %>%
+  dplyr::select(c(id, name, age, team, position, points, sd_pts, floor, ceiling, dropoff, points_vor,
+           floor_vor, ceiling_vor, rank, floor_rank, ceiling_rank, pos_rank, tier, pos_ecr,
+           sd_ecr, uncertainty))
+
+ffanalytics::.Graphs()
